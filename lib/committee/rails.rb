1edfba94136ff2335884df106994e9747863e632
willnet <netwillnet@gmail.com>
2016-09-07 20:30:34 +0900

    initial commit

---
+++ lib/committee/rails.rb
@@ -0,0 +1,7 @@
+require "committee/rails/version"
+
+module Committee
+  module Rails
+    # Your code goes here...
+  end
+end
---
+++ lib/committee/rails/version.rb
@@ -0,0 +1,5 @@
+module Committee
+  module Rails
+    VERSION = "0.1.0"
+  end
+end
