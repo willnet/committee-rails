c290648e10fa1015174842b4c9f10be90eb2959e
willnet <netwillnet@gmail.com>
2016-09-23 11:18:58 +0900

    Refactor rails app for test

--- spec/fake_app/rails_app.rb
+++ spec/fake_app/rails_app.rb
@@ -1,21 +1,14 @@
-# config
 app = Class.new(Rails::Application)
-app.config.secret_token = '964ab2f0fbbb68bc36f3cc487ca296bb8555fac50627924024c245a1599e5265'
+app.secrets.secret_key_base = '964ab2f0fbbb68bc36f3cc487ca296bb8555fac50627924024c245a1599e5265'
 app.config.session_store :cookie_store, key: '_myapp_session'
-app.config.active_support.deprecation = :log
 app.config.eager_load = false
-
-# Rais.root
 app.config.root = File.dirname(__FILE__)
-Rails.backtrace_cleaner.remove_silencers!
 app.initialize!
 
-# routes
 app.routes.draw do
   resources :users
 end
 
-# controllers
 class ApplicationController < ActionController::Base
 end
 
