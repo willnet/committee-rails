771d118238cb3d86459fa9b37d5d80f51b476088
willnet <netwillnet@gmail.com>
2018-04-20 14:39:31 +0900

    Fix broken spec about Rails 5.2.0

--- spec/fake_app/rails_app.rb
+++ spec/fake_app/rails_app.rb
@@ -1,11 +1,10 @@
-app = Class.new(Rails::Application)
-app.secrets.secret_key_base = '964ab2f0fbbb68bc36f3cc487ca296bb8555fac50627924024c245a1599e5265'
-app.config.session_store :cookie_store, key: '_myapp_session'
-app.config.eager_load = false
-app.config.root = File.dirname(__FILE__)
-app.initialize!
+FakeApp = Class.new(Rails::Application)
+FakeApp.config.session_store :cookie_store, key: '_myapp_session'
+FakeApp.config.eager_load = false
+FakeApp.config.root = File.dirname(__FILE__)
+FakeApp.initialize!
 
-app.routes.draw do
+FakeApp.routes.draw do
   resources :users
 end
 
