6b287cf893e33ca3ee71229515e4e67b104a8b5b
ota42y <ota42y@gmail.com>
2018-11-15 23:14:15 +0900

    support committee new options

--- lib/committee/rails/test/methods.rb
+++ lib/committee/rails/test/methods.rb
@@ -4,65 +4,32 @@ module Committee::Rails
       include Committee::Test::Methods
 
       def committee_schema
-        @committee_schema = committee_options[:schema]
-
-        @committee_schema ||= begin
-
-          driver = Committee::Drivers::HyperSchema.new
-          schema_hash = JSON.parse(File.read(Rails.root.join('docs', 'schema', 'schema.json')))
-          driver.parse(schema_hash)
-        end
+        # this code for 2.4.0, committee 3.x use committee_options
+        @committee_schema ||= Committee::Middleware::Base.new(nil, committee_options).send(:get_schema, committee_options)
       end
 
       def committee_options
-        {}
+        if defined?(RSpec) && (options = RSpec.try(:configuration).try(:committee_options))
+          options
+        elsif !defined?(@committee_schema)
+          { schema: default_schema }
+        else
+          # schema_url_prefix method call this but the user overrite comittee_schema, we got error
+          # we can remove in comittee 3.x
+          { schema: committee_schema }
+        end
       end
 
-      def assert_schema_conform
-        @committee_schema ||= begin
-          # The preferred option. The user has already parsed a schema elsewhere
-          # and we therefore don't have to worry about any performance
-          # implications of having to do it for every single test suite.
-          if committee_schema
-            committee_schema
-          else
-            schema = schema_contents
+      def default_schema
+        @default_schema ||= Committee::Drivers.load_from_file(Rails.root.join('docs', 'schema', 'schema.json').to_s)
+      end
 
-            if schema.is_a?(String)
-              warn_string_deprecated
-            elsif schema.is_a?(Hash)
-              warn_hash_deprecated
-            end
+      def request_object
+        request
+      end
 
-            if schema.is_a?(String)
-              schema = JSON.parse(schema)
-            end
-
-            if schema.is_a?(Hash) || schema.is_a?(JsonSchema::Schema)
-              driver = Committee::Drivers::HyperSchema.new
-
-              # The driver itself has its own special cases to be able to parse
-              # either a hash or JsonSchema::Schema object.
-              schema = driver.parse(schema)
-            end
-
-            schema
-          end
-        end
-
-        @committee_router ||= Committee::Router.new(@committee_schema,
-          prefix: schema_url_prefix)
-
-        link, _ = @committee_router.find_request_link(request)
-        unless link
-          invalid_response = "`#{request.request_method} #{request.path_info}` undefined in schema."
-          raise Committee::InvalidResponse.new(invalid_response)
-        end
-
-        if validate_response?(response.response_code)
-          data = JSON.parse(response.body)
-          Committee::ResponseValidator.new(link).call(response.response_code, response.headers, data)
-        end
+      def response_data
+        [response.status, response.headers, response.body]
       end
     end
   end
