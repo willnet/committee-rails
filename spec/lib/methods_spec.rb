d6710dec012851d01d77562422107357b9a87252
willnet <netwillnet@gmail.com>
2020-03-19 14:02:14 +0900

    Update RSpec descriptions

--- spec/lib/methods_spec.rb
+++ spec/lib/methods_spec.rb
@@ -3,29 +3,29 @@ require 'spec_helper'
 describe '#assert_schema_conform', type: :request do
   include Committee::Rails::Test::Methods
 
-  context 'set option' do
+  context 'when set option' do
     before do
       RSpec.configuration.add_setting :committee_options
       RSpec.configuration.committee_options = { schema_path: Rails.root.join('schema', 'schema.json').to_s, old_assert_behavior: false }
     end
 
-    context 'response conform JSON Schema' do
+    context 'and when response conform JSON Schema' do
       it 'pass' do
         post '/users', params: { nickname: 'willnet' }.to_json, headers: { "Content-Type" =>  "application/json" }
         assert_schema_conform
       end
     end
 
-    context "response doesn't conform JSON Schema" do
-      it 'fail' do
+    context "and when response doesn't conform JSON Schema" do
+      it 'raise Committee::InvalidResponse' do
         patch '/users/1', params: { nickname: 'willnet' }.to_json, headers: { "Content-Type" =>  "application/json" }
         expect { assert_schema_conform }.to raise_error(Committee::InvalidResponse)
       end
     end
   end
 
-  context 'not set option' do
-    context 'not override default setting' do
+  context 'when not set option' do
+    context 'and when not override default setting' do
       before do
         RSpec.configuration.committee_options = nil
       end
@@ -36,12 +36,12 @@ describe '#assert_schema_conform', type: :request do
       end
     end
 
-    context 'override default setting' do
+    context 'when override default setting' do
       def committee_options
         { schema_path: Rails.root.join('schema', 'schema.json').to_s, old_assert_behavior: false }
       end
 
-      it 'pass' do
+      it 'use the setting' do
         post '/users', params: { nickname: 'willnet' }.to_json, headers: { "Content-Type" =>  "application/json" }
         assert_schema_conform
       end
